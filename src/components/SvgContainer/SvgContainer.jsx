8d268eaf0e54036bfc820eb3bed5969642f7ba39
Szymon Mecnarowski <szymon.mecnarowski@hewalex.pl>
2019-06-24 07:58:26 +0200

    added horizontal centering, improved matching size

--- src/components/SvgContainer/SvgContainer.jsx
+++ src/components/SvgContainer/SvgContainer.jsx
@@ -8,23 +8,51 @@ class SvgContainer extends Component {
     super(props);
 
     this.groupRef = React.createRef();
+    this.svgRef = React.createRef();
   }
 
   componentDidMount() {
-    this.groupRef.current.setAttribute('transform', this.getGroupScale());
+    this.groupRef.current.setAttribute('transform', this.getGroupTransform() + '');
   }
 
-  getGroupScale() {
-    const width = this.groupRef.current ? this.groupRef.current.getBoundingClientRect().width : 2000;
-    const height = this.groupRef.current ? this.groupRef.current.getBoundingClientRect().height : 2000;
-    const proportion = width > height ? height / width : width / height;
+  getGroupTransform() {
+    const groupBounding = this.groupRef.current.getBoundingClientRect();
+    const svgBounding = this.svgRef.current.getBoundingClientRect();
 
-    return 'scale(' + proportion + ')';
+    const widthProportion = (svgBounding.width - 10) / groupBounding.width;
+    const heightProportion = (svgBounding.height - 10) / groupBounding.height;
+
+    const proportion = groupBounding.width > groupBounding.height ? widthProportion : heightProportion;
+    const factor = (widthProportion > 1 ? -1 : 1);
+    let translation = (groupBounding.left - (groupBounding.width/2)) * factor;
+    if (svgBounding.width - groupBounding.width < 0) {
+      translation = (groupBounding.left - svgBounding.left) * -1;
+    }
+    translation *= 480 / svgBounding.width
+
+    return 'scale(' + proportion + ') translate(' + translation + ')';
+  }
+
+  getStyle() {
+    return {
+      position: 'relative',
+      height: '100%',
+      width: '100%',
+      top: 0
+    };
   }
 
   render() {
+    const width = 580;
+    const height = 480;
     return (
-      <svg viewBox={'-5 -5 ' + 480 + ' ' + 480} width={'100%'}>
+      <svg 
+        ref={this.svgRef}
+        width={width} 
+        height={height} 
+        viewBox={'0 0 ' + width + ' ' + height}
+        style={this.getStyle()}
+      >
         <g ref={this.groupRef}>
           {this.props.children}
         </g>
--- src/index.js
+++ src/index.js
@@ -6,205 +6,205 @@ import { App } from './App'
 import { updateData } from './actions'
 import registerServiceWorker from './registerServiceWorker'
 
-// const config = {
-//   type: 'split',
-//   date: {
-//     fill: '#00b100',
-//     value: '15-12-2017 14:01:50',
-//   },
-//   CWU: {
-//     value: true, // 3 pierwsze bity z 208 + p208_4 === 1
-//     visible: true, //z konfiguracji
-//     type: {
-//       name: 'condenser', //condenser / coil
-//       value: true, //z konfiguracji
-//     },
-//     additionalHeater: {
-//       name: 'automatic_boiler', //heater /automatic_boiler
-//       unit: '',
-//       value: false, //p208_11
-//     },
-//     circulation: {
-//       value: true, //p208_4 > 0 to true
-//       visible: true,
-//     },
-//   },
-//   CO: {
-//     value: true, // 3 pierwsze bity z 208 + p208_4 === 0
-//     visible: true, //z konfiguracji
-//     type: {
-//       name: 'boiler', //noBoiler / boiler
-//       value: false, //z konfiguracji
-//     },
-//     additionalHeater: {
-//       name: 'automatic_boiler', //heater /automatic_boiler
-//       unit: '',
-//       value: true, //p208_12
-//     },
-//   },
-//   p128: {
-//     name: 'T1',
-//     value: 123,
-//     unit: '°C',
-//   },
-//   p132: {
-//     name: 'T2',
-//     value: 33,
-//     unit: '°C',
-//   },
-//   p134: {
-//     name: 'T3',
-//     value: 33,
-//     unit: '°C',
-//   },
-//   p140: {
-//     name: 'T6',
-//     value: 33,
-//     unit: '°C',
-//   },
-//   p146: {
-//     name: 'T9',
-//     value: 33,
-//     unit: '°C',
-//   },
-//   p150: {
-//     name: 'T11',
-//     value: 15,
-//     unit: '°C',
-//   },
-//   p152: {
-//     // temperatureSensor - z konfiguracji
-//     name: 'T12', // temperatureSensor === "digital" ? "" : "T12"
-//     value: 15, // temperatureSensor === "digital" ? (value > 0 ? "ON" : "OFF") : value
-//     unit: '°C', // temperatureSensor === "digital" ? "" : "°C"
-//   },
-//   p154: {
-//     // temperatureSensor === "digital" - z konfiguracji
-//     name: 'T13', // temperatureSensor === "digital" ? "" : "T13"
-//     value: 15, // temperatureSensor === "digital" ? (value > 0 ? "ON" : "OFF") : value
-//     unit: '°C', // temperatureSensor === "digital" ? "" : "°C"
-//   },
-//   p156: {
-//     name: 'T14',
-//     value: 15,
-//     unit: '°C',
-//   },
-//   Output: {
-//     circuit1: {
-//       value: true, //p208_5 > 0
-//       visible: true, //z konfiguracji
-//     },
-//     circuit2: {
-//       value: true, //p208_8 > 0
-//       visible: true, //z konfiguracji
-//     },
-//     circuit3: {
-//       value: true, //p208_10 > 0
-//       visible: true, //z konfiguracji
-//     },
-//     temperatureSensor: {
-//       name: 'digital', //digital lub analog
-//     },
-//     value: true, //p208_5 > 0 || /p208_8 > 0 || /p208_10 > 0,
-//   },
-//   PC1: {
-//     //p322
-//     value: 1,
-//   },
-//   PC2: {
-//     //p372
-//     value: 0,
-//   },
-// }
-
-// POSITION: 0 - woda użytkowa // 1 - inne zastosowanie // 2, 3 - niestandardowe np sch3 na kotle
 const config = {
-  type: 'solar',
+  type: 'split',
   date: {
     fill: '#00b100',
     value: '15-12-2017 14:01:50',
   },
+  CWU: {
+    value: true, // 3 pierwsze bity z 208 + p208_4 === 1
+    visible: false, //z konfiguracji
+    type: {
+      name: 'condenser', //condenser / coil
+      value: true, //z konfiguracji
+    },
+    additionalHeater: {
+      name: 'automatic_boiler', //heater /automatic_boiler
+      unit: '',
+      value: false, //p208_11
+    },
+    circulation: {
+      value: true, //p208_4 > 0 to true
+      visible: true,
+    },
+  },
+  CO: {
+    value: true, // 3 pierwsze bity z 208 + p208_4 === 0
+    visible: false, //z konfiguracji
+    type: {
+      name: 'boiler', //noBoiler / boiler
+      value: false, //z konfiguracji
+    },
+    additionalHeater: {
+      name: 'automatic_boiler', //heater /automatic_boiler
+      unit: '',
+      value: true, //p208_12
+    },
+  },
   p128: {
-    //visible, position
     name: 'T1',
     value: 123,
     unit: '°C',
-    visible: true,
-    position: 0,
-  },
-  p130: {
-    name: 'T2',
-    value: 11,
-    unit: '°C',
-    visible: true,
-    position: 0,
   },
   p132: {
+    name: 'T2',
+    value: 33,
+    unit: '°C',
+  },
+  p134: {
     name: 'T3',
     value: 33,
     unit: '°C',
-    visible: true,
-    position: 0,
   },
-  p134: {
-    name: 'T4',
-    value: 34,
-    unit: '°C',
-    visible: true,
-    position: 0,
-  },
-
-  p152: {
-    name: 'F1',
+  p140: {
+    name: 'T6',
     value: 33,
-    unit: 'l/min',
-    visible: true,
-    position: 0,
+    unit: '°C',
   },
-
+  p146: {
+    name: 'T9',
+    value: 33,
+    unit: '°C',
+  },
+  p150: {
+    name: 'T11',
+    value: 15,
+    unit: '°C',
+  },
+  p152: {
+    // temperatureSensor - z konfiguracji
+    name: 'T12', // temperatureSensor === "digital" ? "" : "T12"
+    value: 15, // temperatureSensor === "digital" ? (value > 0 ? "ON" : "OFF") : value
+    unit: '°C', // temperatureSensor === "digital" ? "" : "°C"
+  },
   p154: {
-    name: 'F2',
-    value: true,
-    unit: 'l/min',
-    visible: true,
-    position: 0,
+    // temperatureSensor === "digital" - z konfiguracji
+    name: 'T13', // temperatureSensor === "digital" ? "" : "T13"
+    value: 15, // temperatureSensor === "digital" ? (value > 0 ? "ON" : "OFF") : value
+    unit: '°C', // temperatureSensor === "digital" ? "" : "°C"
   },
-  p154_0: true,
-  p154_1: true,
-  p154_2: true,
   p156: {
-    name: 'E1',
-    value: 100,
-    unit: '%',
-    visible: true,
-    position: 0,
+    name: 'T14',
+    value: 15,
+    unit: '°C',
   },
-  p170: {
-    name: 'Schemat',
+  Output: {
+    circuit1: {
+      value: true, //p208_5 > 0
+      visible: true, //z konfiguracji
+    },
+    circuit2: {
+      value: true, //p208_8 > 0
+      visible: true, //z konfiguracji
+    },
+    circuit3: {
+      value: true, //p208_10 > 0
+      visible: true, //z konfiguracji
+    },
+    temperatureSensor: {
+      name: 'digital', //digital lub analog
+    },
+    value: true, //p208_5 > 0 || /p208_8 > 0 || /p208_10 > 0,
+  },
+  PC1: {
+    //p322
     value: 1,
   },
-  p136: {
-    name: 'T5',
-    value: 34,
-    unit: '°C',
-    visible: true,
-    position: 0, // 0 - woda użytkowa / 1 - inne zastosowanie(pod zegarem)  / 2 - inne
-  },
-  p138: {
-    name: 'T6',
-    value: 34,
-    unit: '°C',
-    visible: true,
-    position: 0, // 0 - woda użytkowa / 1 - inne zastosowanie(pod zegarem)  / 2 - inne
-  },
-  p292: {
-    name: 'F2',
-    value: 14,
-    unit: 'l/min',
-    visible: true,
-    position: 0,
+  PC2: {
+    //p372
+    value: 0,
   },
 }
+
+// POSITION: 0 - woda użytkowa // 1 - inne zastosowanie // 2, 3 - niestandardowe np sch3 na kotle
+// const config = {
+//   type: 'solar',
+//   date: {
+//     fill: '#00b100',
+//     value: '15-12-2017 14:01:50',
+//   },
+//   p128: {
+//     //visible, position
+//     name: 'T1',
+//     value: 123,
+//     unit: '°C',
+//     visible: true,
+//     position: 0,
+//   },
+//   p130: {
+//     name: 'T2',
+//     value: 11,
+//     unit: '°C',
+//     visible: true,
+//     position: 0,
+//   },
+//   p132: {
+//     name: 'T3',
+//     value: 33,
+//     unit: '°C',
+//     visible: true,
+//     position: 0,
+//   },
+//   p134: {
+//     name: 'T4',
+//     value: 34,
+//     unit: '°C',
+//     visible: true,
+//     position: 0,
+//   },
+
+//   p152: {
+//     name: 'F1',
+//     value: 33,
+//     unit: 'l/min',
+//     visible: true,
+//     position: 0,
+//   },
+
+//   p154: {
+//     name: 'F2',
+//     value: true,
+//     unit: 'l/min',
+//     visible: true,
+//     position: 0,
+//   },
+//   p154_0: true,
+//   p154_1: true,
+//   p154_2: true,
+//   p156: {
+//     name: 'E1',
+//     value: 100,
+//     unit: '%',
+//     visible: true,
+//     position: 0,
+//   },
+//   p170: {
+//     name: 'Schemat',
+//     value: 1,
+//   },
+//   p136: {
+//     name: 'T5',
+//     value: 34,
+//     unit: '°C',
+//     visible: true,
+//     position: 0, // 0 - woda użytkowa / 1 - inne zastosowanie(pod zegarem)  / 2 - inne
+//   },
+//   p138: {
+//     name: 'T6',
+//     value: 34,
+//     unit: '°C',
+//     visible: true,
+//     position: 0, // 0 - woda użytkowa / 1 - inne zastosowanie(pod zegarem)  / 2 - inne
+//   },
+//   p292: {
+//     name: 'F2',
+//     value: 14,
+//     unit: 'l/min',
+//     visible: true,
+//     position: 0,
+//   },
+// }
 
 const run = (config, selector) => {
   const target = document.querySelector(selector)
